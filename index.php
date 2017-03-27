bc7535de2a6f342d40e17801ab7584e45fba5d81
vkoll29 <fuhrer.go@gmail.com>
2017-03-27 15:56:18 +0300

    porfile page included. Any profile can be accessed from the address baar. Sort that

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ if(Session::exists('home')){
 $user = new User();
 if($user->isLoggedIn()){
 ?>
-    <p>Hello <a href="#"><?php echo escape($user->data()->name); ?></a> </p>
+    <p>Hello <a href="profile.php?user=<?php echo escape($user->data()->username); ?>"><?php echo escape($user->data()->name); ?></a> </p>
     <ul>
         <li><a href="update.php">Update Details</a></li>
         <li><a href="changepassword.php">Change your password</a></li>
--- profile.php
+++ profile.php
@@ -1,7 +1,20 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: vkoll29
- * Date: 03/08/17
- * Time: 4:37 PM
- */
+require_once 'core/init.php';
+if(!$username = Input::get('user')){
+    Redirect::to('index.php');
+}else{
+    $user = new User($username);
+    if(!$user->exists()){
+        Redirect::to(404);
+    }else{
+        $data = $user->data();
+    }
+    ?>
+    <div class="details">
+        <h2>Your Details</h2>
+        <h4>Username: <?php echo escape($data->username); ?></h4>
+        <h3>Full name: <?php echo escape($data->name); ?></h3>
+    </div>
+
+<?php
+}
